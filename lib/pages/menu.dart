7c6ffbdfd22bc50ea6bffed2b4d0a3f424148ed0
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-07-19 23:04:41 +0530

    chore: Update menu page layout and add "ADD MENU" button

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../model/Items.dart';
 import '../widgets/menuItemTile.dart';
@@ -12,34 +13,66 @@ class Menu extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80),
+          preferredSize: const Size.fromHeight(80),
           child: Container(
-            margin: EdgeInsets.only(left: 15,right: 15),
+            margin: const EdgeInsets.only(left: 15, right: 15),
             child: Row(
               children: [
                 Container(
-                    height: 35,
-                    child: Image.asset('assets/images/backbutton.png'),),
-                    Spacer(),
-                Container(child: Image.asset('assets/images/logo.png'),),
+                  height: 35,
+                  child: Image.asset('assets/images/backbutton.png'),
+                ),
+                const Spacer(),
+                Container(
+                  child: Image.asset('assets/images/logo.png'),
+                ),
               ],
             ),
           ),
         ),
-        body: Card(
-          color: Colors.white,
-          child: ListView.separated(
-            itemCount: menuItems.length,
-            separatorBuilder: (context, index) => const Divider(
-              height: .5,
-              color: Color(0xff3FD47A),
-              endIndent: 20,
-              indent: 20,
+        body: Column(
+          children: [
+            Expanded(
+              child: Card(
+                color: Colors.white,
+                child: ListView.separated(
+                  itemCount: menuItems.length,
+                  separatorBuilder: (context, index) => const Divider(
+                    height: .5,
+                    color: Color(0xff3FD47A),
+                    endIndent: 20,
+                    indent: 20,
+                  ),
+                  itemBuilder: (context, index) {
+                    return MenuItemTile(menuItems[index]);
+                  },
+                ),
+              ),
             ),
-            itemBuilder: (context, index) {
-              return MenuItemTile(menuItems[index]);
-            },
-          ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0,top: 20), // Add space at the bottom
+              child: Container(
+                height: 55,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: const Color(0xff128B42),
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Center(
+                  child: Text(
+                    'ADD MENU',
+                    style: GoogleFonts.getFont(
+                      'Jaldi',
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
