4fbbca6d31974d4fe6c7e5dbe7826573797488d0
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-08-05 09:17:40 +0530

    refactor: Update main.dart to use SplashScreen as the home widget
    The code changes in main.dart update the home widget to use SplashScreen instead of Tables. This change improves the user experience by displaying a splash screen when the app is launched.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'pages/splash_screen.dart';
 import 'pages/tables.dart';
-
-
-
-
-
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Food App',
-      home: Tables(),
+      home: SplashScreen(),
     );
   }
 }
