7cea2eb3355f6dcc9e2b33e0d90e30b3182b3321
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-08-10 11:02:52 +0530

    ```text refactor: Update splash screen animation
    The code changes in splash_screen.dart refactor the splash screen animation by adding fade-in animations for the logo and text. This improves the visual appeal of the splash screen and provides a smoother transition to the home screen.

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-
 import 'package:BillWiz/pages/home.dart';
 import 'package:flutter/material.dart';
-
-
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,19 +10,54 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _logoController;
+  late AnimationController _textController;
+  late Animation<double> _logoAnimation;
+  late Animation<double> _textAnimation;
+
   @override
   void initState() {
     super.initState();
+
+    // Animation Controllers
+    _logoController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    _textController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // Fade-in Animations
+    _logoAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_logoController);
+    _textAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_textController);
+
+    // Start Animations in Sequence
+    _startAnimationSequence();
+
+    // Navigate to Home after a delay
     Timer(
-      Duration(seconds: 3),
+      Duration(seconds: 4), // Increase delay to allow animation to complete
       () {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const Home()),
         );
       },
     );
+  }
+
+  void _startAnimationSequence() async {
+    await _logoController.forward();
+    await _textController.forward();
+  }
+
+  @override
+  void dispose() {
+    _logoController.dispose();
+    _textController.dispose();
+    super.dispose();
   }
 
   @override
@@ -37,7 +69,8 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           children: [
             const Spacer(),
-            Container(
+            FadeTransition(
+              opacity: _logoAnimation,
               child: Image.asset(
                 'assets/images/logo.png',
                 scale: 1.0,
@@ -45,22 +78,29 @@ class _SplashScreenState extends State<SplashScreen>
               ),
             ),
             const SizedBox(height: 20.0),
-            Text(
-              'Menaria Fast Food',
-              style: GoogleFonts.getFont(
-                'Satisfy',
-                fontWeight: FontWeight.w400,
-                fontSize: 32,
-                color: const Color(0xFF000000),
-              ),
-            ),
-            Text(
-              'We Believe In Taste',
-              style: GoogleFonts.getFont(
-                'Sriracha',
-                fontWeight: FontWeight.w400,
-                fontSize: 18,
-                color: const Color(0xFF128B42),
+            FadeTransition(
+              opacity: _textAnimation,
+              child: Column(
+                children: [
+                  Text(
+                    'Menaria Fast Food',
+                    style: GoogleFonts.getFont(
+                      'Satisfy',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 32,
+                      color: const Color(0xFF000000),
+                    ),
+                  ),
+                  Text(
+                    'We Believe In Taste',
+                    style: GoogleFonts.getFont(
+                      'Sriracha',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: const Color(0xFF128B42),
+                    ),
+                  ),
+                ],
               ),
             ),
             const Spacer(),
